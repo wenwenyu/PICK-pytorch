ac3dc1bed20cdb1eb75f9468fb9ebf82e5c11b82
Wenwen Yu <yuwenwen62@gmail.com>
2020-07-27 15:57:19 +0800

    change to default dataset cfg

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -2,20 +2,20 @@
 # @Author: Wenwen Yu
 # @Created Time: 7/8/2020 9:34 PM
 
-# Entities_list = [
-#     "company",
-#     "address",
-#     "date",
-#     "total"
-# ]
-
 Entities_list = [
-    "ticket_num",
-    "starting_station",
-    "train_num",
-    "destination_station",
+    "company",
+    "address",
     "date",
-    "ticket_rates",
-    "seat_category",
-    "name"
+    "total"
 ]
+
+# Entities_list = [
+#     "ticket_num",
+#     "starting_station",
+#     "train_num",
+#     "destination_station",
+#     "date",
+#     "ticket_rates",
+#     "seat_category",
+#     "name"
+# ]
